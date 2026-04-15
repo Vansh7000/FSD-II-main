3888445151ab2bce5da7a107d30eab8c17544131
Vansh0007 <vanshsingal162@gmail.com>
2026-04-15 22:58:01 +0530

    Exp 6,7,8-Done

---
+++ Experiments/experiment-6/src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// 👇 ADD THIS HERE
+app.get("/", (req, res) => {
+    res.send("Server is running 🚀");
+});
+
+app.use("/api", authRoutes);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
